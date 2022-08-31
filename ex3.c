308ed1ecc769f8546879d6b7bcc6e24f71fa94cd
Marcos Rampaso <marcosbezner@gmail.com>
2022-08-31 14:38:24 -0300

    ex3 finalizado

--- ex3.c
+++ ex3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FILHOS 5
-#define VET_SIZE 10
+#define QTD_FILHOS 2
+#define VET_SIZE 11
 #define SEARCH_FOR 63
 
 int *cria_vetor_aleatorio(int size) // Função que cria um vetor com valores aleatórios
@@ -21,7 +21,7 @@ void imprime_vetor(int size, int *vetor) // Função que imprime o vetor
     {
         if (i == size - 1)
         {
-            printf("%d", vetor[i]);
+            printf("%d\n", vetor[i]);
         }
         else
         {
@@ -30,18 +30,91 @@ void imprime_vetor(int size, int *vetor) // Função que imprime o vetor
     }
 }
 
+int verifica_vetor(int x, int j, int quantidade,int numero_procurado,int* vetor)
+{
+    for (j = x; j < x + quantidade; j++)
+    {
+        if (vetor[j] == numero_procurado)
+        {
+            printf("O valor %d foi encontrado na posicao %d do vetor\n", numero_procurado, j);
+            return 1;
+        }
+    }
+}
+
 int main()
 {
-    int *vetor_a = cria_vetor_aleatorio(20);
+    int *vetor = cria_vetor_aleatorio(20);
 
-    imprime_vetor(VET_SIZE, vetor_a);
+    imprime_vetor(VET_SIZE, vetor);
 
-    int quantidade = VET_SIZE / FILHOS;
-    int resto = VET_SIZE % FILHOS;
+    int quantidade = VET_SIZE / QTD_FILHOS; // quantidade de elementos para cada filho
+    int resto = VET_SIZE % QTD_FILHOS;      // Caso haja resto, ele terá de ser colocado em algum filho
 
-    /*Criar funções:
-    Incialização dos filhos;
-    Buscar elementos, escolher por onde começar etc
-    */
+    // int* teste = cria_vetor_aleatorio(QTD_FILHOS);
+
+    // Declaração das variáveis
+
+    pid_t pid, pid2;
+    int status = 0;
+    int searched_number = SEARCH_FOR;
+    int i = 0, j = 0, x = 0;
+    int controle_quantidade = 0;
+
+    pid = getpid();
+
+    for (i = 0; i < QTD_FILHOS & pid != 0; ++i) 
+    {
+        pid = fork();   //Cria o processo filho
+
+        if (pid >= 0)   
+        {
+            if (pid == 0)
+            {
+                if (i < resto)  //dividindo as partes para os filhos
+                {
+                    controle_quantidade = quantidade + 1;
+                }
+                else
+                {
+                    controle_quantidade = quantidade;
+                }
+
+                if (i < resto)
+                {
+                    x = i * (quantidade);       //Calcula a posição de início
+                }
+                else
+                {
+                    x = i * controle_quantidade + resto; //Coloca o valor do resto no filho (caso tenha resto)
+                }
+
+                verifica_vetor(x, j, controle_quantidade, searched_number, vetor); //Cada filho verifica o vetor com suas respectivas instruções
+                return 0;
+            }
+        }
+        else
+        {
+            printf("ERRO NA CRIAÇÃO DO PROCESSO FILHO\n");
+            return -1;
+        }
+    }
+
+    // Verifica qual filho encontrou o valor procurado
+    if (pid)
+    {
+        x = 0;
+
+        do
+        {
+            pid2 = wait(&status);
+            if (WEXITSTATUS(status) != 0)
+            {
+                printf("Olha so! O filho %d encontrou o valor, parabens!\n", pid2);
+            }
+            x++;
+        } while (x < QTD_FILHOS);
+    }
+
     return 0;
 }
